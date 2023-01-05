efbdf4b6d07357406ed88f87fdb87ec612f18e87
kash-cpu <abdulazeez.kasheem@gmail.com>
2023-01-05 03:46:07 -0800

    correction

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
-
-"""
-function that prints a text with 2 new lines after
-each of these characters: ., ?
+""" text_indentation returns "text" in the specified format:
+2 newlines after each ['.', '?', ':']
 """
 
 
 def text_indentation(text):
+    """ prints "text" with 2 newlines after each of these char: ['.', '?', ':']
+    checks if "text" is a str
+    first loop removes spaces after each required chars
+    second loop adds 2 newlines after each required chars
     """
-    Prints a new line
-    """
-    if not isinstance(text, str):
-        raise TypeError('text must be a string')
-    punc = text.replace('.', '.\n\n')
-    punc = punc.replace('?', '?\n\n')
-    punc = punc.replace(':', ':\n\n')
-    newLine = punc.split('\n')
-    for line in range(len(newLine)):
-        print("{}".format(newLine[line].strip()),
-              end=("" if (line == (len(newLine) - 1)) else '\n'))
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    toCatAfter = ['.', '?', ':']
+
+    # Removes the space after special chars
+    idx = 0
+    for items in text:
+        if items in toCatAfter:
+            if text[idx + 1] == " ":
+                text = text[:idx + 1] + text[idx + 2:]
+        else:
+            idx += 1
+
+    # Cats '\n\n' after the special char with removed space
+    idx = 0
+    for items in text:
+        if items in toCatAfter:
+            text = text[:idx + 1] + '\n\n' + text[idx + 1:]
+            idx += 3
+        else:
+            idx += 1
+
+    print(text, end='')
