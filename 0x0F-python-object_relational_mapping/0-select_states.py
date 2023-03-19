b32702e76f22918b2cd7366a1b2db994cc318c97
kash-cpu <abdulazeez.kasheem@gmail.com>
2023-03-19 07:03:15 -0700

    task solved

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module uses MySQLdb to print out states
+
+Usage:
+    ./filename user pw db
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    args = sys.argv
+    dt = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=args[1],
+        passwd=args[2],
+        db=args[3],
+        charset="utf8")
+    cursor = dt.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    dt.close()
