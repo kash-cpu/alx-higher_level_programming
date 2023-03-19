cb2b397a55f7d19ce6d7cc2ef330e5697248a024
kash-cpu <abdulazeez.kasheem@gmail.com>
2023-03-19 02:08:27 -0700

    correction

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const el in dict) {
+  const key = dict[el];
+  if (key in newDict) {
+    newDict[key].push(el);
   } else {
-    newDict[dict[key]].push(key);
+    newDict[key] = [el];
   }
 }
 console.log(newDict);
